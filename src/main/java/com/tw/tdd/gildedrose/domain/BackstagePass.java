6cfd6534a9cf2a7b8b00b03a957610804a864497
jkxue <jkxupt@163.com>
2020-08-25 00:27:13 +0800

    feat: cover 'given backstagePass with sellIn greater than 5 and less or eq 10 and quality greater than 0 when sellIn minus one then the quality increase two and not more than 50'

--- src/main/java/com/tw/tdd/gildedrose/domain/BackstagePass.java
+++ src/main/java/com/tw/tdd/gildedrose/domain/BackstagePass.java
@@ -9,6 +9,12 @@ public class BackstagePass extends Item{
             if(this.getQuality() + 1 <= 50){
                 this.setQuality(this.getQuality() + 1);
             }
+        }else if(this.getSellIn() > 5 && this.getSellIn() <=10){
+            if(this.getQuality() + 2 <= 50){
+                this.setQuality(this.getQuality() + 2);
+            }else{
+                this.setQuality(50);
+            }
         }
         this.setSellIn(this.getSellIn() - 1);
     }
