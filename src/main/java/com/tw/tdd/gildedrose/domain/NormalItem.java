104f36480fac788ca01d9a363a8eda5819196e6b
jkxue <jkxupt@163.com>
2020-08-24 23:44:35 +0800

    feat: cover 'given normalItem with sellIn less or eq 0 and quality greater than 0 when sellIn minus one then the quality should minus two and greater than 0'

--- src/main/java/com/tw/tdd/gildedrose/domain/NormalItem.java
+++ src/main/java/com/tw/tdd/gildedrose/domain/NormalItem.java
@@ -5,9 +5,17 @@ public class NormalItem extends Item{
     @Override
     public void sellInDecrease() {
 
-        this.setSellIn(this.getSellIn() -1);
-        if(this.getQuality() > 0){
-            this.setQuality(this.getQuality() - 1);
+        if (this.getSellIn() > 0) {
+            if (this.getQuality() > 0) {
+                this.setQuality(this.getQuality() - 1);
+            }
+        }else{
+            if (this.getQuality() > 2) {
+                this.setQuality(this.getQuality() - 2);
+            }else{
+                this.setQuality(0);
+            }
         }
+        this.setSellIn(this.getSellIn() - 1);
     }
 }
