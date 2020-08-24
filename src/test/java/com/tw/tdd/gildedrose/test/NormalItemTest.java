ad5abb22188487cd9f0aa644307b8ea96fde8bf4
jkxue <jkxupt@163.com>
2020-08-24 23:36:13 +0800

    feat: case 'given normalItem with sellIn less or eq 0 and quality greater than 0 when sellIn minus one then the quality should minus two and greater than 0'

--- src/test/java/com/tw/tdd/gildedrose/test/NormalItemTest.java
+++ src/test/java/com/tw/tdd/gildedrose/test/NormalItemTest.java
@@ -33,7 +33,7 @@ public class NormalItemTest {
     }
 
     @Test
-    public void given_normalItem_with_sellIn_greater_than_0_and_quality_is_0_when_sellIn_minus_one_then_the_quality_minus_one() {
+    public void given_normalItem_with_sellIn_greater_than_0_and_quality_is_0_when_sellIn_minus_one_then_the_quality_is_0() {
 
         Integer quality1 = 0;
         Integer sellIn1 = 6;
@@ -52,6 +52,31 @@ public class NormalItemTest {
 
         assertEquals(sellIn1 -1, normalItem1.getSellIn().intValue());
         assertEquals(0, normalItem1.getQuality().intValue());
+
+        assertEquals(sellIn2 -1, normalItem2.getSellIn().intValue());
+        assertEquals(0, normalItem2.getQuality().intValue());
+    }
+
+    @Test
+    public void given_normalItem_with_sellIn_less_or_eq_0_and_quality_greater_than_0_when_sellIn_minus_one_then_the_quality_should_minus_two_and_greater_than_0() {
+
+        Integer quality1 = 6;
+        Integer sellIn1 = 0;
+        Item normalItem1 = new NormalItem();
+        normalItem1.setQuality(quality1);
+        normalItem1.setSellIn(sellIn1);
+
+        Integer quality2 = 1;
+        Integer sellIn2 = -1;
+        Item normalItem2 = new NormalItem();
+        normalItem2.setQuality(quality2);
+        normalItem2.setSellIn(sellIn2);
+
+        normalItem1.sellInDecrease();
+        normalItem2.sellInDecrease();
+
+        assertEquals(sellIn1 -1, normalItem1.getSellIn().intValue());
+        assertEquals(quality1 - 2, normalItem1.getQuality().intValue());
 
         assertEquals(sellIn2 -1, normalItem2.getSellIn().intValue());
         assertEquals(0, normalItem2.getQuality().intValue());
