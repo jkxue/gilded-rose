beaf04ecb7dabf95b9906bc8fad282baf363f2a8
jkxue <jkxupt@163.com>
2020-08-25 00:31:33 +0800

    feat: case 'given backstagePass with sellIn greater than 0 and less or eq 5 and quality greater than 0 when sellIn minus one then the quality increase 3 and not more than 50'

--- src/test/java/com/tw/tdd/gildedrose/test/BackstagePassTest.java
+++ src/test/java/com/tw/tdd/gildedrose/test/BackstagePassTest.java
@@ -55,4 +55,29 @@ public class BackstagePassTest {
         assertEquals(sellIn2 -1, backstagePass2.getSellIn().intValue());
         assertEquals(50, backstagePass2.getQuality().intValue());
     }
+
+    @Test
+    public void given_backstagePass_with_sellIn_greater_than_0_and_less_or_eq_5_and_quality_greater_than_0_when_sellIn_minus_one_then_the_quality_increase_3_and_not_more_than_50() {
+
+        Integer quality1 = 47;
+        Integer sellIn1 = 5;
+        Item backstagePass1 = new BackstagePass();
+        backstagePass1.setQuality(quality1);
+        backstagePass1.setSellIn(sellIn1);
+
+        Integer quality2 = 48;
+        Integer sellIn2 = 1;
+        Item backstagePass2 = new BackstagePass();
+        backstagePass2.setQuality(quality2);
+        backstagePass2.setSellIn(sellIn2);
+
+        backstagePass1.sellInDecrease();
+        backstagePass2.sellInDecrease();
+
+        assertEquals(sellIn1 -1, backstagePass1.getSellIn().intValue());
+        assertEquals(quality1 + 3, backstagePass1.getQuality().intValue());
+
+        assertEquals(sellIn2 -1, backstagePass2.getSellIn().intValue());
+        assertEquals(50, backstagePass2.getQuality().intValue());
+    }
 }
